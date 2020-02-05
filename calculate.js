ab5128b63b3067b6aad627db8017a0635d33317e
Patrick <patrick.hurley@codeimmersives.com>
2020-02-05 12:21:56 -0500

    initial commit

--- calculate.js
+++ calculate.js
@@ -2,9 +2,28 @@
  * YOUR CODE HERE *
  ******************/
 
+function calculate(num1,num2,operator){
+  num1 = Number(num1)
+  num2 = Number(num2)
+  if (operator === '+' || operator === 'plus' || operator=== 'added to'){
+return num1 + num2
+  }else if (operator === '-' || operator=== 'minus' || operator === 'subtracted from'){
+    return num1 - num2
+}else if (operator === 'x' || operator=== 'times' || operator==='X' || operator=== 'multiplied by'){
+  return num1 * num2
+}
+else if (operator === '/' || operator=== '' || operator === 'divided by'){
+  return num1 / num2
+}
+  else if (operator === '%' || operator==='modulus' || operator==='mod'){
+    return num1 % num2
+  }
+  else {
+  return "Sorry, that's not a mathematical operation!" 
+}
+}
 
- 
-
+console.log(calculate('6', '5', '+'));
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
